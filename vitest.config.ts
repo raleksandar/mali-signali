10382ee26f5a247656940bac9af0fee4d3459508
Aleksandar Ružičić <aleksandar@ruzicic.info>
2026-04-01 14:55:55 +0200

    Upgrade Vitest and test environment tooling

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
         coverage: {
             provider: 'v8',
             include: ['src/**/*.ts'],
-            exclude: ['**/*.test.ts'],
+            exclude: ['**/*.test.ts', 'package/**'],
             reporter: ['text', 'html', 'lcov'],
         },
     },
